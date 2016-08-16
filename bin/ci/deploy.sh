bb05c293d53a0f8ea40a4cae6f8470247c555c5e
Anders Evenrud <andersevenrud@gmail.com>
2016-08-16 20:32:19 +0200

    Updated travis deployment routine

--- bin/ci/deploy.sh
+++ bin/ci/deploy.sh
@@ -6,13 +6,19 @@ set -x # Show the output of the following commands (useful for debugging)
 
 #if [ $TRAVIS_BRANCH == 'master' ] ; then
 if [ $TRAVIS_BRANCH == 'travis-deploy' ] ; then
-    mkdir _deploy
-    cd _deploy
-    git init
+    # Set up remote
     git remote add deploy "osjs@builds.os.js.org:/home/osjs/CI"
     git config user.name "Travis CI"
     git config user.email "anders.evenrud+travisCI@gmail.com"
-    git add .
+
+
+    # Create new blank branch
+    git checkout --orphan travis-ci
+    git rm --cached -r .
+    rm .gitignore
+
+    # Add files for deployment
+    git add -f dist src/server src/conf vfs vendor
     git commit -m "Automatic Travis CI Deploy"
     git push --force deploy master
 else
